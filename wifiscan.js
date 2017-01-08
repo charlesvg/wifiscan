e3f8c1087144130a913f40a83593c781f0fa722a
charlesvg <charles.vangoidsenhoven@gmail.com>
2017-01-08 20:49:38 +0100

    added distance measurement

--- wifiscan.js
+++ wifiscan.js
@@ -1,9 +1,17 @@
 var pcap=require('pcap');
-pcap.createSession("wlp2s0b1", '(type mgt) and (type mgt subtype probe-req )').
+pcap.createSession('wlp2s0b1', '(type mgt) and (type mgt subtype probe-req )').
         on('packet', function (raw_packet) {
-            //console.log(JSON.stringify(pcap.decode.packet(raw_packet)));
-                with(pcap.decode.packet(raw_packet).payload.ieee802_11Frame)
-                        if (type == 0 && subType == 4)
-                                console.log("Probe request",shost, "-> ",bssid);
+            var payload = pcap.decode.packet(raw_packet).payload;
+            var frame = payload.ieee802_11Frame;
+            if (frame.type == 0 && frame.subType == 4) {
+                console.log("Source", frame.shost.addr, "Signal:", payload.signalStrength, "Distance", calculateDistance(payload.signalStrength, payload.frequency));
+            }
         }
 );
+
+
+function calculateDistance(signalLevelInDb, freqInMHz) {
+        // http://stackoverflow.com/questions/11217674/how-to-calculate-distance-from-wifi-router-using-signal-strength
+        var exp = (27.55 - (20.0 * (Math.log(freqInMHz) / Math.log(10))) + Math.abs(signalLevelInDb)) / 20.0;
+        return Math.pow(10.0, exp);
+}
